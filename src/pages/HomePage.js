861f2fb404a6de609d3d0d7a8100d7bec418994f
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2024-10-30 15:08:17 -0600

    add separator component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import MainLayout from '@/components/layout/MainLayout';
+import { Separator } from '@/components/ui/separator';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
@@ -16,10 +17,13 @@ const HomePage = () => {
     <MainLayout>
       <div className="grid gap-4">
         {books.map((book) => (
-          <div className="flex gap-4">
-            <img src={book.coverImage} alt={book.title} className="w-14" />
-            <h1 key={book.id}>{book.title}</h1>
-          </div>
+          <>
+            <div className="flex gap-4">
+              <img src={book.coverImage} alt={book.title} className="w-14" />
+              <h1 key={book.id}>{book.title}</h1>
+            </div>
+            <Separator className="my-2" />
+          </>
         ))}
       </div>
     </MainLayout>
