e3f43b1fb7d2d0b35b33274a60e4fa6fd34c311c
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2024-10-30 14:59:18 -0600

    list books

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,29 @@
 import MainLayout from '@/components/layout/MainLayout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
-  return <MainLayout></MainLayout>;
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/books');
+      setBooks(response.data);
+    };
+    fetchData();
+  }, []);
+  console.log(books);
+  return (
+    <MainLayout>
+      <div className="grid gap-4">
+        {books.map((book) => (
+          <div className="flex gap-4">
+            <img src={book.coverImage} alt={book.title} className="w-14" />
+            <h1 key={book.id}>{book.title}</h1>
+          </div>
+        ))}
+      </div>
+    </MainLayout>
+  );
 };
 
 export default HomePage;
