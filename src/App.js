b0c5aada6aa5f983a3b01a830d2933b36e09cd51
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2024-11-30 21:52:53 -0700

    Revert "fix: delete unused route"
    This reverts commit c0780c586a5b20f4bba608b93b8147e84e15aa96.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './global.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import BookPage from './pages/BookPage';
+import BookEditPage from './pages/BookEditPage';
 import BookNewPage from './pages/BookNewPage';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/books/:id" element={<BookPage />} />
+          <Route path="/book-edit/:id" element={<BookEditPage />} />
           <Route path="/book-new/" element={<BookNewPage />} />
         </Routes>
       </AuthProvider>
